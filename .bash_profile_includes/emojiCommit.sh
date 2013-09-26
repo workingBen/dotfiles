28e71472d13a845d2d91ce369a30f9e1898b7caa
Ben Pellow <bpellow@walmartlabs.com>
2013-09-26 10:59:27 -0700

    refactor gcm and gcam :octocat:

--- .bash_profile_includes/emojiCommit.sh
+++ .bash_profile_includes/emojiCommit.sh
@@ -1,28 +1,22 @@
-function gcm {
+function emojiCommit {
   emojis=(`cat ~/.bash_profile_includes/emojis`)
-
   RANDOM=$$$(date +%s)
 
   selectedEmoji=${emojis[$RANDOM % ${#emojis[@]} ]}
 
   emoji_message="$1 $selectedEmoji"
+}
+
+function gcm {
+  emojiCommit "$1"
 
   echo git commit -m \"$emoji_message\"
-
   git commit -m "$emoji_message"
 }
 
 function gcam {
-  emojis=(`cat ~/.bash_profile_includes/emojis`)
-
-  RANDOM=$$$(date +%s)
-
-  selectedEmoji=${emojis[$RANDOM % ${#emojis[@]} ]}
-
-  emoji_message="$1 $selectedEmoji"
+  emojiCommit "$1"
 
   echo git commit -am \"$emoji_message\"
-
   git commit -am "$emoji_message"
 }
-
